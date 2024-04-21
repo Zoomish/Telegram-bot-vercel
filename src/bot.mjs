6246646bd8880cb28ded6b47478cb1c2e54575df
Zoom <egor147536987@gmail.com>
2024-04-21 20:09:12 +0300

    "Updated bot.on function to send a message with a reply markup"
    The diff includes adding a step in the promise chain to return a new message sent to the user with a reply markup. The message content is 'AAAAAAAAAAAAAAAA

--- src/bot.mjs
+++ src/bot.mjs
@@ -16,8 +16,10 @@ bot.on(["/start", "/back"], (msg) => {
     .then((response) => response.json())
     .then((json) => {
       console.log(json.title);
+      return bot.sendMessage(msg.from.id, "AAAAAAAAAAAAAAAAAAAAA", {
+        replyMarkup,
+      });
     });
-  return bot.sendMessage(msg.from.id, 'AAAAAAAAAAAAAAAAAAAAA', { replyMarkup });
 });
 
 // Buttons
